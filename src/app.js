ae64590c42fcc2fc073a9eb6a0a68408fdaf9039
Luis Giuliani <154923735+jrgiuliani@users.noreply.github.com>
2024-03-16 02:25:27 +0000

    Agregué comentario que explica el código

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
-  let pronoun = ['the', 'our'];
-  let adj = ['great', 'big'];
-  let noun = ['jogger', 'racoon'];
-  for (let a in pronoun){
-    for (let b in adj){
-      for (let c in noun){
-        console.log(pronoun[a]+adj[b]+noun[c]+".com");
-      }
+//Generador de dominios. combina los strings de los arrays y agrega el .com
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+for (let a in pronoun) {
+  for (let b in adj) {
+    for (let c in noun) {
+      console.log(pronoun[a] + adj[b] + noun[c] + ".com");
     }
   }
+}
